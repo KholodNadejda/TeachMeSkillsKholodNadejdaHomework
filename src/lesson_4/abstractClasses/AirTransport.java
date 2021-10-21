2e197e82f01f22fbd99ca232ff7499cc5a82ea8e
Nadejda Kholod <cthuthbev@gmail.com>
2021-10-21 14:08:34 +0300

    Completed lesson4 homework

--- src/lesson_4/abstractClasses/AirTransport.java
+++
@@ -1,6 +0,0 @@
-package lesson_4.abstractClasses;
-
-public class AirTransport extends Transport {
-    public double wingspan;
-    public double minRunwayLength;
-}
--- src/lesson_4/abstractClasses/LandTransport.java
+++
@@ -1,6 +0,0 @@
-package lesson_4.abstractClasses;
-
-public class LandTransport extends Transport {
-    public int numberOfWheels;
-    public double fuelConsumption;
-}
